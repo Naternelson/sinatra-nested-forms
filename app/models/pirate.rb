59b04c6144d9a302f413a906de6a86b3759195ea
Nate <7nater.nelson@gmail.com>
2021-02-25 12:20:42 -0700

    commit tree fab173488036901bfdb3858b83f48aecd7d438b5

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+    def initialize(attributes={})
+        # attributes.each do |key, value| self.send(("#{key}="),value) if defined? ("#{key}=").to_sym}
+        self.name=attributes[:name]
+        self.weight = attributes[:weight]
+        self.height = attributes[:height]
+        self.class.all << self
+    end
+    def self.all
+        @@all
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+    def initialize(attributes)
+        attributes.each {|key, value| self.send(("#{key}="),value)}
+        self.class.all << self
+    end
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
